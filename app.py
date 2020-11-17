393d3920ef356a2646f18848acac4c43d4ecde74
Benjamin Barr <benwbarr@gmail.com>
2020-11-16 23:30:05 -0700

    worked on app.py file

--- app.py
+++ app.py
@@ -1,35 +1,52 @@
-# 1. import Flask
-from flask import Flask
-import datetime as dt
+  
 import numpy as np
 import pandas as pd
-
+import datetime as dt
+import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
 from flask import Flask, jsonify
 
-# Database
-engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+engine = create_engine("sqlite:///Resources/hawaii.sqlite", connect_args={'check_same_thread': False})
+
+
 Base = automap_base()
 Base.prepare(engine, reflect=True)
-                      
+Base.classes.keys()
+
 Measurement = Base.classes.measurement
 Station = Base.classes.station
 
-# Session
 session = Session(engine)
 
-# Flask Setup
-app = Flask (__name__)
-                       
-# Flask Routes
+#weather app
+app = Flask(__name__)
+
+
+latestDate = (session.query(Measurement.date)
+                .order_by(Measurement.date.desc())
+                .first())
+latestDate = list(np.ravel(latestDate))[0]
+
+latestDate = dt.datetime.strptime(latestDate, '%Y-%m-%d')
+latestYear = int(dt.datetime.strftime(latestDate, '%Y'))
+latestMonth = int(dt.datetime.strftime(latestDate, '%m'))
+latestDay = int(dt.datetime.strftime(latestDate, '%d'))
+
+yearBefore = dt.date(latestYear, latestMonth, latestDay) - dt.timedelta(days=365)
+yearBefore = dt.datetime.strftime(yearBefore, '%Y-%m-%d')
+
+
+
+
 @app.route("/")
-def welcome():
-    return (
-        f"All available API routes:<br/>"
-        f"/api/v1.0/precipitation<br/>"
-        f"/api/v1.0/stations<br/>"
-        f"/api/v1.0/tobs<br/>"
-        f"/api/v1.0/start<br/>"
-        f"/api/v1.0/start/end<br/>")                     
+def home():
+    return (f"Welcome<br/>"
+            f"Available Routes:<br/>"
+            f"/api/v1.0/stations ------ Weather observation stations<br/>"
+            f"/api/v1.0/precipitaton -- Preceipitation data<br/>"
+            f"/api/v1.0/temperature --- Temperature data<br/>"
+            f"/api/v1.0/start<br/>"
+            f"/api/v1.0/start/end<br/>")
+
