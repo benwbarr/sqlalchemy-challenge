98851dc08d2356faa3a6373753e07e85e8b084e3
Benjamin Barr <benwbarr@gmail.com>
2020-11-16 22:20:09 -0700

    added app.py

--- app.py
+++ app.py
@@ -11,3 +11,25 @@ from flask import Flask, jsonify
 
 # Database
 engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+                      
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Session
+session = Session(engine)
+
+# Flask Setup
+app = Flask (__name__)
+                       
+# Flask Routes
+@app.route("/")
+def welcome():
+    return (
+        f"All available API routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/start<br/>"
+        f"/api/v1.0/start/end<br/>")                     
